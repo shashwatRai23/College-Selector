73fae6a376547d80cd2b0ba17835b7d2aacdd63b
Shashwat <shashwat.rai.mec20@itbhu.ac.in>
2024-06-03 19:29:27 +0530

    Added the heading

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchComponent from './Components/SearchComponent';
 function App() {
   return (
     <div className="App">
+      <h1>College Selector</h1>
       <SearchComponent/>
     </div>
   );
